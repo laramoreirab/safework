61d41d0f4322a4caa48978c2874c654a622b32e6
laramoreirab <laramoreiraba@gmail.com>
2025-11-19 17:19:09 -0300

    integração cadastro

---
+++ public/js/scriptCadastro.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form_cadastro')
+form.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+    const nomeEmpresa = document.getElementById('nome_cadastro').value
+    const emailEmpresa = document.getElementById('email_cadastro').value
+    const senhaEmpresa = document.getElementById('senha_cadastro').value
+
+})
