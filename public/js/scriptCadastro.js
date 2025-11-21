05312f22ed0cdf0b00694f6f934f37b0db335ff1
Lara Moreira Barreto <larabarretomoreira.005@gmail.com>
2025-11-20 22:58:11 -0300

    integração cadastro

--- public/js/scriptCadastro.js
+++ public/js/scriptCadastro.js
@@ -6,4 +6,9 @@ form.addEventListener('submit', async(e) =>{
     const emailEmpresa = document.getElementById('email_cadastro').value
     const senhaEmpresa = document.getElementById('senha_cadastro').value
 
+    const res = await fetch('http://localhost:3000/cadastro', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}
+        body: JSON.stringify()
+    })
 })
