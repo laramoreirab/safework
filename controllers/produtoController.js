4ddf1de448020a9dac5dedaa0782b3f349f9a568
Lara Moreira Barreto <larabarretomoreira.005@gmail.com>
2025-11-27 19:50:01 -0300

    adicionando produto controller

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -1,3 +1,138 @@
 // onde o adminin poderá adicionar, excluir, atualizar ou listas os produtos
 //e onde os usuários poderão procurar os produtos por id
 
+import ProdutoModel from '../models/produtoModel.js'
+import { fileURLToPath } from 'url'
+import path from 'path'
+// import { removerArquivoAntigo } from '../middlewares/uploadMiddleware.js'
+
+const _filename = fileURLToPath(import.meta.url)
+const _dirname = path.dirname(_filename)
+
+// Controller para operações com produtos
+
+class ProdutoController {
+
+    // GET /produtos  para listar todos os produtos (paginação)
+    static async ListarProdutos(req, res) {
+        try {
+
+            let pagina = parseInt(req.query.pagina) || 1;
+            let limite = parseInt(req.query.limite) || 40;
+
+            if (pagina <= 0) {
+                return res.status(400).json({
+                    sucesso: false,
+                    erro: 'Página Inválida',
+                    mensagem: 'A página deve ser um número maior que zero'
+                })
+            }
+            if (limite <= 0) {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Limite inválido',
+                    mensagem: 'O limite deve ser um número maior que zero'
+                })
+            }
+
+            const limiteMaximo = parseInt(process.env.PAGINACAO_LIMITE_MAXIMO) || 100
+            if (limite > limiteMaximo) {
+                res.status(400).json({
+                    sucesso: false,
+                    erro: 'Limite Inválido',
+                    mensagem: `O limite deve ser um numero entre 1 e ${limiteMaximo}`
+                })
+            }
+
+            const offset = (pagina - 1) * limite;
+
+            const resultado = await ProdutoModel.listarTodos(limite, offset);
+
+            res.status(200).json({
+                sucesso: true,
+                dados: resultado.produtos,
+                paginacao: {
+                    pagina: resultado.pagina,
+                    limite: resultado.limite,
+                    total: resultado.total,
+                    totalPaginas: resultado.totalPaginas
+                }
+            })
+        } catch (err) {
+            console.error('Erro ao listar produtos:', err)
+            res.status(500).json({
+                sucesso: false,
+                erro: 'Erro interno do servidor',
+                mensagem: 'Não foi possível listar os produtos'
+            })
+        }
+    }
+
+    // GET /produtos/:id
+    static async buscarPorId(req, res) {
+        try {
+            const { id } = req.params
+
+            if (!id || isNaN(id)) { // caso for diferente ou não for um numero 
+                return res.status(400).json({
+                    sucesso: false,
+                    erro: 'ID Inválido',
+                    mensagem: 'O ID deve ser um número válido'
+                })
+            }
+
+            const produto = await ProdutoModel.buscarPorId(id);
+
+            if(!produto) {
+                return res.status(400).json({
+                    sucesso: false,
+                    erro: 'Produto não encontrado',
+                    mensagem: `Produto com ID ${id} não foi encontrado`
+                })
+            }
+            
+            res.status(200).json({
+                suceso: true,
+                dados: produto
+            })
+        } catch(err) {
+            console.error('Erro ao buscar produto', err)
+            res.status(500).json({
+                sucesso: false,
+                erro: 'Erro interno do servidor',
+                mensagem: "Não foi possível buscar o produto"
+            })
+        }
+    }
+
+    // GET /produtos/:categoria
+    static async buscarPorCategoria(req, res) {
+        try {
+            const { categoria } = req.params
+            
+            const produto = await ProdutoModel.buscarPorCategoria(categoria);
+
+            if (!produto) {
+                return res.status(400).json({
+                    sucesso: false,
+                    erro: 'Produto não encontrado',
+                    mensagem: `Produto com categoria ${id} não foi encontrado`
+                })
+            }
+
+            res.status(200).json({
+                suceso: true,
+                dados: produto
+            })
+        } catch (err) {
+            console.error('Erro ao buscar produto', err)
+            res.status(500).json({
+                sucesso: false,
+                erro: 'Erro interno do servidor',
+                mensagem: "Não foi possível buscar o produto"
+            })
+        }
+    }
+}
+
+export default  ProdutoController 
