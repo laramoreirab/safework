12dda6ec9cb566a46d6296709d0331e225c3975f
Arian <arianmonteiromendes@gmail.com>
2025-12-05 11:16:41 -0300

    Funcionalidade: Aprimoramento da página de informações do produto e do painel de administração com campos obrigatórios
    - Adicionados IDs aos elementos em infoprodutos.html para melhor acessibilidade e manipulação de JavaScript.
    
    - Atualizado o parágrafo de descrição do produto com um ID para gerenciamento de conteúdo dinâmico.
    
    - Introduzido um novo ID para a seção de interesse do produto para possíveis melhorias futuras.
    
    - Incluída uma tag de script para infoprodutos.js para habilitar funcionalidades adicionais.
    
    - Tornados obrigatórios diversos campos de entrada em painel-adm.html para garantir a integridade dos dados durante as atualizações de produtos.
    
    - Adicionada uma div de paginação ao painel de administração para melhorar a navegação.

--- public/js/adm.js
+++ public/js/adm.js
@@ -15,9 +15,8 @@ form.addEventListener('submit', async (e) => {
     const estoqueProduto = document.getElementById('qtd_prod-new').value
     const descricaoProduto = document.getElementById('descricao_prod-new').value
     const marcaProduto = document.getElementById('marca_prod-new').value
+    const dataDeValidade = document.getElementById('datavalidade_prod-new').value
     const InputImg = document.getElementById('img_prod-upload')
-
-    console.log('esse é o nome do produto: ', nomeProduto)
     if (estoqueProduto < 1) {
         e.preventDefault()
         alert('O estoque deve ter pelo menos 1 item')
@@ -38,6 +37,7 @@ form.addEventListener('submit', async (e) => {
     formData.append('estoque', estoqueProduto)
     formData.append('descricao', descricaoProduto)
     formData.append('marca', marcaProduto)
+    formData.append('validade', dataDeValidade)
 
     if (InputImg.files.length > 0) {
         formData.append('imagem', InputImg.files[0])
@@ -59,57 +59,103 @@ form.addEventListener('submit', async (e) => {
 })
 // EDITAR PRODUTO
 
-async function atualizarProduto() {
-
-    const nomeProduto = document.getElementById('atualiza-nome_prod-new').value
-    const categoriaProduto = document.getElementById('atualiza-categoria_prod').value
-    const caProduto = document.getElementById('atualiza-ca_prod').value
-    const precoProduto = document.getElementById('atualiza-preco_prod').value
-    const estoqueProduto = document.getElementById('atualiza-qtd_prod').value
-    const descricaoProduto = document.getElementById('descricao_prod-new').value
-    const InputImg = document.getElementById('atualiza-img_prod-upload')
-
-    const formData = new FormData()
-    formData.append('nome', nomeProduto)
-    formData.append('tipo', categoriaProduto)
-    formData.append('ca', caProduto)
-    formData.append('preco', precoProduto)
-    formData.append('estoque', estoqueProduto)
-    formData.append('descricao', descricaoProduto)
-
-    if (InputImg.files.length > 0) {
-        formData.append('imagem', InputImg.files[0])
-    }
+// async function atualizarProduto() {
 
 
-    const response = await fetch('/api/produtos/atualizar', {
-        method: 'PUT',
-        body: formData
-    })
 
-    const data = await response.json()
-    if (data.sucesso) {
-        alert('Produto criado com sucesso!')
-        window.location.reload()
-    } else {
-        alert('Erro ao criar produto: ' + data.mensagem)
-    }
-}
+//     const nomeProduto = document.getElementById('atualiza-nome_prod-new').value
+//     const categoriaProduto = document.getElementById('atualiza-categoria_prod').value
+//     const caProduto = document.getElementById('atualiza-ca_prod').value
+//     const precoProduto = document.getElementById('atualiza-preco_prod').value
+//     const estoqueProduto = document.getElementById('atualiza-qtd_prod').value
+//     const descricaoProduto = document.getElementById('descricao_prod-new').value
+//     const dataDeValidade = document.getElementById('datavalidade_prod-new').value
+//     const marcaProduto = document.getElementById('marca_prod-new').value
+//     const InputImg = document.getElementById('atualiza-img_prod-upload')
+//     const formData = new FormData()
+//     formData.append('nome', nomeProduto)
+//     formData.append('tipo', categoriaProduto)
+//     formData.append('ca', caProduto)
+//     formData.append('preco', precoProduto)
+//     formData.append('estoque', estoqueProduto)
+//     formData.append('descricao', descricaoProduto)
+//     formData.append('marca', marcaProduto)
+//     formData.append('validade', dataDeValidade)
+//     console.log(`essa é a data de validade: `, dataDeValidade)
+
+//     if (InputImg.files.length > 0) {
+//         formData.append('imagem', InputImg.files[0])
+//     }
+
+
+//     const response = await fetch('/api/produtos/atualizar', {
+//         method: 'PUT',
+//         body: formData
+//     })
+
+//     const data = await response.json()
+//     if (data.sucesso) {
+//         alert('Produto criado com sucesso!')
+//         window.location.reload()
+//     } else {
+//         alert('Erro ao criar produto: ' + data.mensagem)
+//     }
+// }
 
 const searchInputProdutos = document.getElementById('searchProdutos')
 renderizarProdutos()
 
+
+// mostra um alerta caso produto estiver perto da validade
+function alertaDeValidade() {
+    fetch('api/produtos/listar')
+        .then(res => res.json())
+        .then(data => {
+            const produtos = data.dados
+            produtos.forEach(p => {
+                const dataAtual = new Date() // data atual
+                const dataValidade = new Date(p.validade) // validade de cada produto
+                const difeMs = dataValidade - dataAtual // diferença em milissegundos
+                const ms = 1000 * 60 * 60 * 24 // milissegundos em 1 dia
+                const difeDias = difeMs / ms // diferença em dias
+                const difeAnos = difeMs / (ms * 365) // diferença em Anos
+
+                if (difeAnos <= 3 && difeAnos >= 0) {
+                    alert(
+                        `⚠️ Atenção!\n\n` +
+                        `O produto "${p.nome}" está próximo da validade.\n\n` +
+                        `📅 Validade: ${dataValidade.toLocaleDateString("pt-BR")}\n` +
+                        `⏳ Faltam apenas ${Math.floor(difeDias)} dias!\n\n` +
+                        `Por favor, verifique o estoque. ✅`
+                    );
+                }
+                if (dataValidade < dataAtual) {
+                    alert(
+                        `❌ Produto vencido!\n\n` +
+                        `O produto "${p.nome}" já está fora da validade.\n\n` +
+                        `📅 Validade expirada em: ${dataValidade.toLocaleDateString("pt-BR")}\n` +
+                        `⚠️ Por favor, retire-o do estoque para garantir a segurança dos clientes. 🛑`
+                    );
+                }
+            }
+
+            )
+        });
+}
 //  mostrar produtos na tabela
-function renderizarProdutos() { // função para puxar todos os produtos ja formatados para pagina produto
-    fetch('/api/produtos/listar') // rota que puxa todos os produtos do banco de dados
+function renderizarProdutos(pagina = 1) { // função para puxar todos os produtos ja formatados para pagina produto
+    fetch(`/api/produtos/listar?pagina=${pagina}&limite=10`) // rota que puxa todos os produtos do banco de dados
         .then(response => response.json())
         .then(data => {
             console.log('produtos', data)
+            const paginacao = data.paginacao
+            const totalProduto = data.paginacao.total
             const produtos = data.dados; // armazena os dados recebidos da API na variável produtos
             const container = document.getElementById("produtos-container"); // seleciona o container onde os produtos serão exibidos
-            const modalExcluirProd = document.getElementById("modalExcluirProd")
-            const modalEditarProd = document.getElementById('modalEditarProd')
+            container.querySelectorAll('.eachproduto').forEach(el => el.remove())
             produtos.forEach(produto => { // percorre todos os registros do banco de dados produtos
+                const dataISO = produto.validade
+                const data = new Date(dataISO)
                 const bloco = document.createElement("tr"); // cria um elemento div
                 bloco.className = "eachproduto"; // nome da classe do bloco é produto
 
@@ -124,7 +170,7 @@ function renderizarProdutos() { // função para puxar todos os produtos ja form
                 </td>
                 <td class="preco_prod">R$ ${produto.preco}</td>
                 <td>
-                10-02-2025
+                ${data.toLocaleDateString("pt-BR")}
                 </td>
                 <td class="status_prod">
                 <p class="status_ativo">
@@ -162,6 +208,9 @@ function renderizarProdutos() { // função para puxar todos os produtos ja form
                 <p class="estoque-vermelho">${produto.estoque} un.</p>
                 </td>
                 <td class="preco_prod">R$ ${produto.preco}</td>
+                <td>
+                ${data.toLocaleDateString("pt-BR")}
+                </td>
                 <td class="status_prod">
                 <p class="status_ativo">
                 ativo
@@ -191,10 +240,29 @@ function renderizarProdutos() { // função para puxar todos os produtos ja form
                 container.appendChild(bloco); // fala para adicionar a div estilizada dentro do container (no caso, adicionar essa div dentro da main)
             })
 
-            const contarProdutos = produtos.length; // conta a quantidade de produtos no banco de dados
-            document.getElementById('qtd-produtos').innerText = contarProdutos; // mostra a quantidade de produtos na página de administração
+            // renderiza botões de paginação
+            const paginacaoDiv = document.getElementById("paginacao");
+            paginacaoDiv.innerHTML = `
+    <button id="btnAnterior" ${paginacao.pagina <= 1 ? "disabled" : ""}>⬅️ Anterior</button>
+    <span>Página ${paginacao.pagina} de ${paginacao.totalPaginas}</span>
+    <button id="btnProxima" ${paginacao.pagina >= paginacao.totalPaginas ? "disabled" : ""}>Próxima ➡️</button>
+`;
+
+            document.getElementById("btnAnterior")?.addEventListener("click", () => {
+                renderizarProdutos(paginacao.pagina - 1);
+            });
+            document.getElementById("btnProxima")?.addEventListener("click", () => {
+                renderizarProdutos(paginacao.pagina + 1);
+            });
+
+            document.getElementById('qtd-produtos').innerText = totalProduto; // mostra a quantidade de produtos na página de administração
         })
 }
+
+
+// mostra um alerta caso produto estiver perto da validade
+alertaDeValidade()
+
 
 // mostra os produtos da tabela após pesquisar algum item
 function filtroProduto() {
@@ -202,6 +270,10 @@ function filtroProduto() {
     fetch('api/produtos/listar')
         .then(res => res.json())
         .then(data => {
+            const paginacaoDiv = document.getElementById("paginacao");
+            if (searchTermProd.length != 0) {
+                paginacaoDiv.innerHTML = ''
+            }
             const container = document.getElementById("produtos-container"); // seleciona o container onde os produtos serão exibidos
             const produtos = data.dados // pega todos os produtos do banco de dados
             const campos = ["nome", "tipo", "ca"]
@@ -209,6 +281,8 @@ function filtroProduto() {
             container.querySelectorAll('.eachproduto').forEach(el => el.remove())
 
             produtoFiltrado.forEach(produto => {
+                const dataISO = produto.validade
+                const data = new Date(dataISO)
                 const bloco = document.createElement("tr"); // cria um elemento div
                 bloco.className = "eachproduto"; // nome da classe do bloco é produto
 
@@ -223,8 +297,8 @@ function filtroProduto() {
                         <p class="estoque-verde">${produto.estoque} un.</p> 
                     </td>
                     <td class="preco_prod">R$ ${produto.preco}</td>
-                                        <td>
-                    10-02-2025
+                    <td>
+                        ${data.toLocaleDateString("pt-BR")}
                     </td>
                     <td class="status_prod">
                         <p class="status_ativo">
@@ -262,6 +336,9 @@ function filtroProduto() {
                         <p class="estoque-vermelho">${produto.estoque} un.</p>
                     </td>
                     <td class="preco_prod">R$ ${produto.preco}</td>
+                    <td>
+                        ${data.toLocaleDateString("pt-BR")}
+                    </td>
                     <td class="status_prod">
                         <p class="status_ativo">
                             ativo
@@ -327,6 +404,7 @@ modalEditarProd.addEventListener('show.bs.modal', () => {
         const estoqueProduto = document.getElementById('atualiza-qtd_prod').value
         const descricaoProduto = document.getElementById('atualiza-descricao_prod').value
         const marcaProduto = document.getElementById('atualiza-marca_prod').value
+        const dataDeValidade = document.getElementById('atualiza-datavalidade_prod').value
         const InputImg = document.getElementById('atualiza-img_prod-upload')
 
         console.log('esse é o nome do produto: ', nomeProduto)
@@ -353,6 +431,7 @@ modalEditarProd.addEventListener('show.bs.modal', () => {
         formData.append('estoque', estoqueProduto)
         formData.append('descricao', descricaoProduto)
         formData.append('marca', marcaProduto)
+        formData.append('validade', dataDeValidade)
         formData.append('img', imgDoProd)
 
         if (InputImg.files.length > 0) {
@@ -527,45 +606,54 @@ async function listarUsuarios() {
         })
 }
 
- let idDoUser = null
+let idDoUser = null
 
-    excluirUsuario.addEventListener('show.bs.modal', () => { // pega o id ao abrir o modal excluirUser 
-        const botao = event.relatedTarget;
-        const usuarioId = botao.getAttribute('data-id')
-        idDoUser = usuarioId
-        console.log(idDoUser)
+excluirUsuario.addEventListener('show.bs.modal', () => { // pega o id ao abrir o modal excluirUser 
+    const botao = event.relatedTarget;
+    const usuarioId = botao.getAttribute('data-id')
+    idDoUser = usuarioId
+    console.log(idDoUser)
 
-    })
+})
 
-    const botaoExcluirUser = document.getElementById('submit-delete_usuario')  // botão de excluir user
-    botaoExcluirUser.onclick = function () { // evento ao clicar no botão de excluir user 
-        try {
-            fetch(`/usuarios/${idDoUser}`, {
-                method: 'DELETE'
-            })
+const botaoExcluirUser = document.getElementById('submit-delete_usuario')  // botão de excluir user
+botaoExcluirUser.onclick = async function () { // evento ao clicar no botão de excluir user 
+    try {
+        const res = await fetch(`/usuarios/${idDoUser}`, {
+            method: 'DELETE'
+        })
+
+        const data = await res.json()
+
+        if (res.ok && data.sucesso) {
             alert('Usuário excluído com sucesso!')
             return location.reload()
-        } catch (err) {
-            console.error('Erro excluir usuário', err)
+        } else {
+            alert('Erro ao excluir usuário: ' + (data.mensagem || 'Verifique os dados'));
         }
 
+    } catch (err) {
+        console.error('Erro excluir usuário', err)
     }
 
+}
 
-    // CRIAR USUÁRIO
 
-    const formUser = document.getElementById('criarUser')
+// CRIAR USUÁRIO
 
-    try {
-        formUser.addEventListener('submit', async (e) => {
-            e.preventDefault()
+const formUser = document.getElementById('criarUser')
 
-            const nomeUser = document.getElementById('nome_user-new').value
-            const emailUser = document.getElementById('email_user-new').value
-            const telefoneUser = document.getElementById('tel_user-new').value
-            const senhaUser = document.getElementById('senha_user-new').value
+try {
+    formUser.addEventListener('submit', async (e) => {
+        e.preventDefault()
 
-            fetch(`/usuarios`, {
+        const nomeUser = document.getElementById('nome_user-new').value
+        const emailUser = document.getElementById('email_user-new').value
+        const telefoneUser = document.getElementById('tel_user-new').value
+        const senhaUser = document.getElementById('senha_user-new').value
+        const tipoDeUser = document.getElementById('tipo_user-new').value
+        try {
+            const response = await fetch(`/usuarios`, {
                 method: 'POST',
                 headers: { 'Content-type': 'application/json' },
                 body: JSON.stringify({
@@ -573,32 +661,43 @@ async function listarUsuarios() {
                     email: emailUser,
                     telefone: telefoneUser,
                     senha: senhaUser,
-                    tipo: 'admin'
+                    tipo: tipoDeUser
                 })
             })
-            alert('Usuário criado com sucesso!')
-            return location.reload()
-        })
-    } catch (err) {
-        console.error("não foi possivel enviar esse usuário: ", err)
-    }
+
+            const data = await response.json();
+
+            if (response.ok && data.sucesso) {
+                alert('Usuário criado com sucesso!');
+                location.reload();
+            } else {
+                alert('Erro ao criar usuário :  ' + (data.mensagem || 'Verifique os dados'));
+            }
+        } catch (err) {
+            return console.log(err)
+        }
+
+    })
+} catch (err) {
+    console.error("não foi possivel enviar esse usuário: ", err)
+}
 
 //  SEARCH DOS USUÁRIOS
 const searchInputUser = document.getElementById('searchUser')
 
 async function filtrarUsuarios() {
     fetch('/usuarios')
-    .then(res => res.json())
-    .then(data => {
-        tabelaUser.querySelectorAll('.eachUser').forEach(el => el.remove())
-        const usuarios = data.dados
-        const searchTermUser = searchInputUser.value.toLowerCase().trim()
-        const usuarioFiltro =  usuarios.filter(usuario => String(usuario.id).includes(searchTermUser))   
-        
-        usuarioFiltro.forEach(usuario => {
-            const bloco = document.createElement('tr')
-            bloco.className = 'eachUser'
-            bloco.innerHTML = `
+        .then(res => res.json())
+        .then(data => {
+            tabelaUser.querySelectorAll('.eachUser').forEach(el => el.remove())
+            const usuarios = data.dados
+            const searchTermUser = searchInputUser.value.toLowerCase().trim()
+            const usuarioFiltro = usuarios.filter(usuario => String(usuario.id).includes(searchTermUser))
+
+            usuarioFiltro.forEach(usuario => {
+                const bloco = document.createElement('tr')
+                bloco.className = 'eachUser'
+                bloco.innerHTML = `
                                                         <td class="nome_user">
                                                             <!-- Colocar aqui o nome do user -->
                                                             ${usuario.nome}
@@ -633,9 +732,9 @@ async function filtrarUsuarios() {
                                                             </div>
                                                         </td>
             `
-            tabelaUser.appendChild(bloco)
+                tabelaUser.appendChild(bloco)
+            })
         })
-    })
-} 
+}
 searchInputUser.addEventListener('input', listarUsuarios)
 searchInputUser.addEventListener('input', filtrarUsuarios)
